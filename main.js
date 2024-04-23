5b74e8eb4cc59186d64537d6f55ca3cdbce91011
Rakhshan174 <160410026+Rakhshan174@users.noreply.github.com>
2024-04-23 06:27:10 -0700

    Q 19 TS/JS for 45 assignment

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Dinner Guest: Working with one of the program from programs Exerices 14 through 18, print a message
+// indicating the number of people you are inviting to dinner
+let invitations = ["Ali", "Bilal"];
+let count_invitations = invitations.length;
+console.log(`${count_invitations}, people will come to the dinner`);
---
+++ main.ts
@@ -0,0 +1,5 @@
+// Dinner Guest: Working with one of the program from programs Exerices 14 through 18, print a message
+// indicating the number of people you are inviting to dinner
+let invitations: string[] =["Ali", "Bilal"]
+let count_invitations : number = invitations.length 
+console.log(`${count_invitations}, people will come to the dinner`);
